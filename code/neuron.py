8a01aa3bbac895a67e09a56dc1b04f2d521b8a0a
aragorn5249 <125681005+aragorn5249@users.noreply.github.com>
2023-05-31 06:06:26 +0200

    Update neuron.py

--- code/neuron.py
+++ code/neuron.py
@@ -1,81 +1,77 @@
 import numpy as np
 
-def initialize_parameters():
-    """
-Function is responsible for initializing the parameters (weights and biases) of a neural network. 
-In this case, the function is designed for a neural network 
-with a single hidden layer 
-and an input layer of size 16,384 neurons.
-
-    """
-    parameters = {}
-    
-    parameters['W1'] = np.random.randn(1, 16384) * 0.01
-    parameters['b1'] = np.zeros((1, 1))
-    
-    return parameters
-
-def sigmoid(Z):
-    """
-    Apply the sigmoid activation function to the input Z.
-    """
-    A = 1 / (1 + np.exp(-Z))
-    return A
-
-def linear_activation_forward(A_prev, W, b):
-    """
-    Perform the forward propagation step for a single layer.
-    Cache needed for the storing of valuse fo BP
-    """
-    Z = np.dot(W, A_prev) + b
-    A = sigmoid(Z)
-    cache = (A_prev, W, b)
-    
-    return A, cache
-
-def L_layer_model(X, Y, learning_rate, num_iterations, print_cost=False):
-    """
-    Train the neural network with L layers.
-    """
-    np.random.seed(1)
-    costs = []
-        
-    parameters = initialize_parameters()
-    
-    for i in range(num_iterations):
-        
-        AL, cache = linear_activation_forward(X, parameters['W1'], parameters['b1'])
-        
+class NeuralNetwork:
+    def __init__(self, input_size):
+        self.input_size = input_size
+        self.parameters = {}
         """
-        Compute cost
+        I did as you advised me. so this part is where "claass" crated with the proper data. 
+        self.parameters is an empty dixtionary, but it should get data during the training
         """
+    
+    def initialize_parameters(self):
+        self.parameters['W1'] = np.random.randn(1, 16384) * 0.01
+        self.parameters['b1'] = np.zeros((1, 1))
+    """
+    initializes the weight W1 with 16384 neurons and the bias b1 with zeros. In theory W1 could be -  np.random.randn(1, self.input_size) * 0.01
+    these parameters will be used during the training or forward propagation of the neural network.
+    """
+    def sigmoid(self, Z):
+        A = 1 / (1 + np.exp(-Z))
+        return A
+
+    def linear_activation_forward(self, A_prev, W, b):
+        Z = np.dot(W, A_prev) + b
+        A = self.sigmoid(Z)
+        cache = (A_prev, W, b, Z, A)
+        
+        return A, cache
+    """
+    This method performs the forward propagation step for a single layer in the neural network
+    """
+    def compute_cost(self, AL, Y):
         cost = -np.mean(Y * np.log(AL) + (1 - Y) * np.log(1 - AL))
-        
-        """
-        Backward propagation
-        """
-        dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
-        dZ = dAL * AL * (1 - AL)
-        dW = np.dot(dZ, cache[0].T) / X.shape[1]
+        return cost
+    """
+    It calculates cost, which should show how well the neural network's . 
+    The goal of training the network is to minimize this cost function through optimization algorithms.
+    """
+    def backward_propagation(self, dAL, cache):
+        A_prev, W, b, Z, A = cache
+        dZ = dAL * A * (1 - A)
+        dW = np.dot(dZ, A_prev.T) / A_prev.shape[1]
         db = np.mean(dZ, axis=1, keepdims=True)
+        dA_prev = np.dot(W.T, dZ)
         
-        """
-        Parameters update
-        """
-        parameters['W1'] -= learning_rate * dW
-        parameters['b1'] -= learning_rate * db
-        
-        """
-        This is for the cost print every 100 itterations.
-        """
-        if print_cost and i % 100 == 0:
-            print("Cost after iteration {}: {}".format(i, cost))
-        if print_cost and i % 100 == 0:
-            costs.append(cost)
-    
-    return parameters
+        return dA_prev, dW, db
+    """
+    This BP function calculates the gradients of the cost function with respect to the parameters of the neural network using the chain rule. 
+    These gradients are important for updating the parameters during the optimization process, such as in gradient descent or other optimization algorithms.
+    """
+    def update_parameters(self, dW, db, learning_rate):
+        self.parameters['W1'] -= learning_rate * dW
+        self.parameters['b1'] -= learning_rate * db
+    """
+    function applies the gradient descent optimization algorithm to update the parameters of the neural network
+    """
+    def train(self, X, Y, learning_rate, num_iterations, print_cost=False):
+        np.random.seed(1)
+        self.initialize_parameters()
+        costs = []
 
-"""
-The code represents a neural network with a single hidden layer consisting of fully connected neurons. 
-In this implementation, the input layer has 16,384 neuronsThe output layer is a single neuron.
-"""
+        for i in range(num_iterations):
+            AL, cache = self.linear_activation_forward(X, self.parameters['W1'], self.parameters['b1'])
+            cost = self.compute_cost(AL, Y)
+            dAL = - (np.divide(Y, AL) - np.divide(1 - Y, 1 - AL))
+            dA_prev, dW, db = self.backward_propagation(dAL, cache)
+            self.update_parameters(dW, db, learning_rate)
+
+            if print_cost and i % 100 == 0:
+                print("Cost after iteration {}: {}".format(i, cost))
+            if print_cost and i % 100 == 0:
+                costs.append(cost)
+        
+        return self.parameters
+    """
+    This suppose to be training process. but not sure if it is ok
+    """
