652bbbf960730f1311fe95ae70bbb140168c44cf
aragorn5249 <125681005+aragorn5249@users.noreply.github.com>
2023-06-02 20:52:13 +0200

    Update cost_function.py

--- code/cost_function.py
+++ code/cost_function.py
@@ -1,1 +1,22 @@
 # here goes the code for the cost function
+
+def compute_cost(AL, Y):
+    """
+    Implement the cost function
+
+    Arguments:
+    AL -- probability vector corresponding to the label predictions, shape (1, number of examples)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    cost -- cross-entropy cost
+    """
+
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    cost = - (1 / m) * np.sum(np.multiply(Y, np.log(AL)) + np.multiply(1 - Y, np.log(1 - AL)))
+
+    cost = np.squeeze(cost)  # To make sure the cost's shape is what we expect (e.g. turns [[17]] into 17).
+
+    return cost
