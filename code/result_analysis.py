dab76c604e5532ecc0d1ba8b571435cc867c5cd4
aragorn5249 <125681005+aragorn5249@users.noreply.github.com>
2023-06-02 20:54:37 +0200

    Update result_analysis.py

--- code/result_analysis.py
+++ code/result_analysis.py
@@ -1,1 +1,19 @@
 # here goes the code for the result analysis
+
+def result_analysis(parameters, X, Y):
+    """
+    Analyze the results of the trained model
+
+    Arguments:
+    parameters -- a dictionary containing the trained parameters
+    X -- input data of shape (input size = 2, number of examples = 200)
+    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), shape (1, number of examples)
+
+    Returns:
+    predictions -- vector of predictions of the model (red: 0 / blue: 1)
+    """
+
+    A2, cache = forward_propagation(X, parameters)
+    predictions = (A2 > 0.5)
+
+    return predictions
