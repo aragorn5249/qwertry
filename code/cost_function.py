877ccdfe4497259a8cde71df6a4ae2e036779481
aragorn5249 <125681005+aragorn5249@users.noreply.github.com>
2023-06-03 09:25:12 +0200

    Update cost_function.py

--- code/cost_function.py
+++ code/cost_function.py
@@ -1,25 +1,29 @@
 # here goes the code for the cost function
+import numpy as np
+
 
 def compute_cost(Yhat, Y):
     """
     Implement the cross-entropy cost function
- 
+
     Arguments:
     Yhat -- probability vector corresponding to the label predictions, shape (1, number of examples)
     Y -- true "label" vector (for example: containing 0 if non-cat, 1 if cat), shape (1, number of examples)
- 
+
     Returns:
     cost -- cross-entropy cost
     """
- 
+
     m = Y.shape[1]
- 
-        # Compute loss from AL and Y.
-    logprobs = np.dot(Y, np.log(Yhat).T) + np.dot((1-Y), np.log(1-Yhat).T)
- 
-    cost = (-1./m) * logprobs 
- 
-    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
-    assert(cost.shape == ())
- 
+
+    # Compute loss from AL and Y.
+    logprobs = np.dot(Y, np.log(Yhat).T) + np.dot((1 - Y), np.log(1 - Yhat).T)
+
+    cost = (-1.0 / m) * logprobs
+
+    cost = np.squeeze(
+        cost
+    )  # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert cost.shape == ()
+
     return cost
